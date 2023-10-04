5d2f54f70d258279aab73768cc474fd8cd114455
chukwukaoranile <prosperbinary@gmail>
2023-10-04 22:25:40 +0100

    Structure for SQLite database and creation of  a django model for the Bug app

--- bug/admin.py
+++ bug/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import bug
+
 
 # Register your models here.
+admin.site.register(bug)
--- bug/models.py
+++ bug/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here
+class bug(models.Model):
+    BUG_TYPES = [
+        ('error', 'Error'),
+        ('feature', 'New Feature'),
+        ('enhancement', 'Enhancement'),
+        ('other', 'Other'),
+
+    ]
+
+    STATUS_CHOICES = [
+        ('todo', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+
+    ]
+
+    description = models.TextField()
+    bug_type = models.CharField(max_length=35, choices=BUG_TYPES, default='error')
+    report_date = models.DateField(default=datetime.now)
+    status = models.CharField(max_length=35, choices=STATUS_CHOICES, default='todo')
+
+    def __str__(self):
+        return self.description
